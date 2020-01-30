ebe5b3a38e69fc6ef91584753ab6fe8db5b6bd4b
Michael Bernier <michael@bernier.im>
2020-01-29 22:26:43 -0500

    Add back handling for search

--- app/src/main/java/im/bernier/movies/search/SearchFragment.kt
+++ app/src/main/java/im/bernier/movies/search/SearchFragment.kt
@@ -4,10 +4,15 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
+import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import im.bernier.movies.R
 import im.bernier.movies.databinding.FragmentSearchBinding
 import im.bernier.movies.datasource.Repository
 
@@ -29,11 +34,22 @@ class SearchFragment : Fragment() {
         binding = FragmentSearchBinding.inflate(layoutInflater,container, false)
         binding.viewModel = viewModel
         binding.lifecycleOwner = viewLifecycleOwner
-        adapter = SearchResultAdapter(listOf())
+        adapter = SearchResultAdapter(listOf()) {id ->
+            findNavController().navigate(R.id.movieFragment, Bundle().apply { putLong("movie", id) })
+        }
         binding.recyclerViewSearchResult.layoutManager = LinearLayoutManager(context)
         binding.recyclerViewSearchResult.adapter = adapter
         Repository.searchResult.observe(viewLifecycleOwner, Observer {
             adapter.update(it)
+        })
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner, object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                if (binding.recyclerViewSearchResult.isVisible) {
+                    (binding.root as MotionLayout).transitionToStart()
+                } else {
+                    findNavController().popBackStack()
+                }
+            }
         })
         return binding.root
     }
@@ -41,4 +57,5 @@ class SearchFragment : Fragment() {
     companion object {
         fun newInstance() = SearchFragment()
     }
+
 }
