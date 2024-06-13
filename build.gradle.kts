0b5dd29260696cf3bc8414ee7929a5d79d033ea3
Michael Bernier <michael@bernier.im>
2024-06-12 21:32:56 -0400

    gradle updates, compose updates, lib updates

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
     alias(libs.plugins.compose.compiler) apply false
     alias(libs.plugins.org.jetbrains.kotlin.android) apply false
+    alias(libs.plugins.dependency.analysis)
 }
 
 buildscript {
