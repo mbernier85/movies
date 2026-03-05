11c36834a8dfccdbb86792be9aac7c5a4b861d10
Michael Bernier <michael@bernier.im>
2026-03-04 21:39:14 -0500

    update agp to 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
     alias(libs.plugins.android.library) apply false
     alias(libs.plugins.google.ksp) apply false
     alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.org.jetbrains.kotlin.android) apply false
     alias(libs.plugins.dependency.analysis)
 }
 
